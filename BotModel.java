4c91882bcf413de6c0f268b9da4b39c796c443e3
khaptonstall <khaptonstall@Kyles-MacBook-Pro.local>
2014-11-16 23:59:57 -0600

    Added base class outline

--- BotModel.java
+++ BotModel.java
@@ -3,6 +3,13 @@ package comp343proj1;
 import java.util.Observable;
 
 public class BotModel extends Observable{
+	public Point loc;
+	public int state; //Shows whether holding debree or free to pick up
+	
+	BotModel(Point loc, int state){
+		loc = this.loc;
+		state = this.state;
+	}
 	
 
 }
